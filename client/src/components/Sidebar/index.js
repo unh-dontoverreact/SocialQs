cec25f2f1b49ff5c6f23de63c3f555ac7e7d0b2e
Mike Galarneau <mbgalarneau@gmail.com>
2019-02-27 20:29:29 -0500

    changed sidebar anchor tags to react links to prevent full page refresh blowing away our app state

--- client/src/components/Sidebar/index.js
+++ client/src/components/Sidebar/index.js
@@ -1,76 +1,96 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import M from "materialize-css/dist/js/materialize.min.js";
 import "materialize-css/dist/css/materialize.min.css";
-import User from "./User"
-import Calendar from 'react-calendar';
+import User from "./User";
+import Calendar from "react-calendar";
 
 class Sidebar extends Component {
-
   constructor(props) {
     super(props);
 
-    this.state = { 
-      date : new Date(),
+    this.state = {
+      date: new Date()
     };
   }
 
   componentDidMount() {
-      let elem = document.querySelector(".sidenav");
-      M.Sidenav.init(elem, {
-          edge: "left",
-          inDuration: 250
-      });
+    let elem = document.querySelector(".sidenav");
+    M.Sidenav.init(elem, {
+      edge: "left",
+      inDuration: 250
+    });
   }
 
-  onChange = date => this.setState({ date })
+  onChange = date => this.setState({ date });
 
   render() {
-      return (
-        <div id="sidebar">
-          <ul id="slide-out" className="sidenav">
-              <li />
-              {/* User Profile Section */ }
-              <User user={this.props.user}/>
-              <li>
-                  <div className="divider" />
-              </li>
+    return (
+      <div id="sidebar">
+        <ul id="slide-out" className="sidenav">
+          <li />
+          {/* User Profile Section */}
+          <User user={this.props.user} />
+          <li>
+            <div className="divider" />
+          </li>
 
-              {/* Contact Section */ }
-              <li><a className="subheader" href="/">Contacts</a></li>
-              <li>
-                  <a href="/contacts">
-                      <i className="material-icons waves-effect">person_add</i>Manage Contacts
-                  </a>
-              </li>
-              <li>
-                  <div className="divider" />
-              </li>
+          {/* Contact Section */}
+          <li>
+            <Link to={`/`} activeClassName="subheader">
+              Contacts
+            </Link>
+          </li>
+          <li>
+            <Link
+              to={`/contacts`}
+              activeClassName="material-icons waves-effect"
+            >
+              <i className="material-icons waves-effect">person_add</i>Manage
+              Contacts
+            </Link>
+          </li>
+          <li>
+            <div className="divider" />
+          </li>
 
-              {/* Events Section */ }
-              <li><a className="subheader" href="/">Events</a></li>
+          {/* Events Section */}
+          <li>
+            <Link to={`/`} activeClassName="subheader">
+              Events
+            </Link>
+          </li>
 
-              <li><a href="#!"><i className="material-icons waves-effect">event_note</i>List Events</a></li>
-              <li>
-                  <div className="divider" />
-              </li>
+          <li>
+            <Link to={`/`} activeClassName="material-icons waves-effect">
+              <i className="material-icons waves-effect">event_note</i>List
+              Events
+            </Link>
+          </li>
+          <li>
+            <div className="divider" />
+          </li>
 
-              {/* Events Section */ }
-              <li><a href="/landing" onClick={this.props.logoutHandler}><i className="material-icons">exit_to_app</i>Logout</a></li>
-              <li>
-                  <div className="divider" />
-              </li>
+          {/* Events Section */}
+          <li>
+            <Link to={`/landing`} onClick={this.props.logoutHandler}>
+              <i className="material-icons">exit_to_app</i>Logout
+            </Link>
+          </li>
+          <li>
+            <div className="divider" />
+          </li>
 
-              { /* Event Calendar */ }
-              <Calendar
-                onChange={this.onChange}
-                value={this.state.date}
-              />
-          </ul>
+          {/* Event Calendar */}
+          <Calendar onChange={this.onChange} value={this.state.date} />
+        </ul>
 
-          { /* Hamburger Menu */ }
-          <a href="/" data-target="slide-out" className="sidenav-trigger"><i className="material-icons">menu</i></a>
-        </div>
-      );
+        {/* Hamburger Menu */}
+        <a href="/" data-target="slide-out" className="sidenav-trigger">
+          <i className="material-icons">menu</i>
+        </a>
+      </div>
+    );
   }
 }
 
