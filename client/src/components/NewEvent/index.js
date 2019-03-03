26d33cfad7a711e89491d0d9ab6ed786e4a87469
FELICIA WAGER <felicia@FELICIAs-MBP.hsd1.ma.comcast.net>
2019-03-03 10:48:47 -0500

    converted add new event form to a react-materialize modal

--- client/src/components/NewEvent/index.js
+++ client/src/components/NewEvent/index.js
@@ -1,14 +1,20 @@
 import React from "react";
 import "materialize-css/dist/css/materialize.min.css";
 import "./style.css";
+import { Modal, Button, Icon } from "react-materialize";
 
 class NewEvent extends React.Component {
     render() {
         return (
-            
-                <div className="new-event-display card z-depth-2 center-align">
-                    <h4>Create New Event </h4>
-
+            <Modal
+                className="new-event-display card z-depth-2 center-align"
+                header="Create New Event"
+                trigger={
+                    <Button>
+                        <Icon>add_circle_outline</Icon>
+                    </Button>
+                }
+            >
                     <form>
                         <div className="input-field">
                             <input
@@ -36,10 +42,13 @@ class NewEvent extends React.Component {
                         </div>
                     </form>
 
-                    <button className="white-text waves-effect waves-light btn #4a148c purple darken-4 z-depth-5" onClick={this.props.enterNewEvent}>
+                    <button
+                        className="white-text waves-effect waves-light btn #4a148c purple darken-4 z-depth-5"
+                        onClick={this.props.enterNewEvent}
+                    >
                         Create
                     </button>
-                </div>
+            </Modal>
         );
     }
 }
