ab33804e9a46d77ef9673e4b59bac76f1de1cd86
Cheyra <staiercm@gmail.com>
2019-03-04 23:27:07 -0500

    minor changes

--- client/src/components/LandingPageSideBar/index.js
+++ client/src/components/LandingPageSideBar/index.js
@@ -1,49 +1,11 @@
 import React from "react";
 import "./index.css";
-<<<<<<< HEAD
-=======
 //import photo from "../../../public/assets/images/mormonfamilyreunion"
->>>>>>> d720c8091aa9a053fea11d803a518adabb896b0d
 
 const LandingPageSideBar = props => (
     <div className="row  all-landing-page-info">
         <div className="Landing-Page-Pitch-display card col s6">
             <h2> Are you a Social Misfit?</h2>
-<<<<<<< HEAD
-
-            <h5> Do you struggle with remembering important dates?</h5>
-            <h5>
-                Do you find time slipping by and realize you haven’t reached out
-                to your family or friends in months?
-            </h5>
-
-            <h5>
-                Have you ever picked up the phone to call someone and had a
-                moment of panic realizing you have no idea what to say or you
-                forgot their significant others name?
-            </h5>
-
-            <h3>
-                If you answered yes to any of these questions, this is the site
-                for you!
-            </h3>
-        </div>
-
-        <div className="col cards s6">
-            <h2> Social Q’s is A Personal Relationship Manager </h2>
-            <h5>
-                It allows you to keep track of your connections even if they've
-                never touched a computer.{" "}
-            </h5>
-            <h5>It displays a timeline of upcoming important events. </h5>
-            <h5>
-                It gives you gentle reminders to "cue" you when an event
-                important to you is coming up.
-            </h5>
-            <h5>
-                It allows you to set interval reminders to connect with someone.
-            </h5>
-=======
             <h3> Do you struggle with... </h3>
             <h5>Remembering important dates?</h5>
             <h5>Keeping up with your family or friends?</h5>
@@ -62,7 +24,6 @@ const LandingPageSideBar = props => (
                 Gentle reminders "cue" you when an important event is coming up.
             </h5>
             <h5>Set interval reminders to connect with someone.</h5>
->>>>>>> d720c8091aa9a053fea11d803a518adabb896b0d
 
             <div className="row">
                 <div className="col s8">
@@ -82,12 +43,9 @@ const LandingPageSideBar = props => (
                 </div>
             </div>
         </div>
-<<<<<<< HEAD
-=======
         <div className="image2">
             <img src="assets/images/mfrpng.png" alt="beach people" />
         </div>
->>>>>>> d720c8091aa9a053fea11d803a518adabb896b0d
     </div>
 );
 
--- client/src/components/UserProfile/index.js
+++ client/src/components/UserProfile/index.js
@@ -4,20 +4,6 @@ import "./style.css";
 
 function UserProfile(props) {
     return (
-<<<<<<< HEAD
-        <div id="userProfile">
-            <h4>Welcome {props.user.firstName}!</h4>
-            <img
-                className="circle"
-                id="mainProfilePhoto"
-                src="http://i.pravatar.cc/300"
-                alt="Profile"
-            />
-
-            <p>Last Login: 01/01/2019</p>
-            <p>Contacts: 27</p>
-            <p>Events: 83</p>
-=======
         <div className="row" id="userProfile">
             <h4>Welcome {props.user.firstName}!</h4>
 
@@ -35,7 +21,6 @@ function UserProfile(props) {
                 <p>Contacts: 27</p>
                 <p>Events: 83</p>
             </div>
->>>>>>> d720c8091aa9a053fea11d803a518adabb896b0d
         </div>
     );
 }
