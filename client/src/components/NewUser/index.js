d32d346e9aa804dfb63b35d58b3477f95eda8d1d
Mike Galarneau <mbgalarneau@gmail.com>
2019-03-06 11:50:16 -0500

    replaced default profile picks with use customizable links

--- client/src/components/NewUser/index.js
+++ client/src/components/NewUser/index.js
@@ -1,44 +1,124 @@
-import React from "react";
-import "./index.css"
+import React, { Component } from "react";
+import "./index.css";
+import { Button, Input } from "react-materialize";
 
+class NewUser extends Component {
+  render() {
+    return (
+      <div>
+        <div className="valign-wrapper center-align new-user-container">
+          <div className="new-user-display card z-depth-2">
+            <h3>Create your Account </h3>
 
-const NewUser = props => (
-<div className = "valign-wrapper center-align new-user-container">
-    <div className= "new-user-display card z-depth-2">
- 
-   <h3>Create your Account </h3>
-    
-   <form autoComplete="on">
-   <div className="input-field">
-          <input placeholder ="Enter your first name" name = "firstName" id="first-name"  className=" .center-align" onChange={props.handleInputChange}/>
+            <form autoComplete="on">
+              <div className="input-field">
+                <Input
+                  placeholder="Enter your first name"
+                  s={6}
+                  name="firstName"
+                  id="first-name"
+                  onChange={this.props.handleInputChange}
+                  className=" .center-align"
+                />
+                <span
+                  className="helper-text"
+                  data-error="wrong"
+                  data-success="right"
+                >
+                  First Name
+                </span>
+              </div>
 
-          <span className="helper-text"  data-error="wrong" data-success="right" >First Name</span>
+              <div className="input-field">
+                <Input
+                  placeholder="Enter your last name"
+                  name="lastName"
+                  id="last-name"
+                  className=" .center-align"
+                  onChange={this.props.handleInputChange}
+                />
+                <span
+                  className="helper-text"
+                  data-error="wrong"
+                  data-success="right"
+                >
+                  Last Name
+                </span>
+              </div>
+
+              <div className="input-field">
+                <Input
+                  placeholder="example@example.com"
+                  name="username"
+                  id="email"
+                  type="email"
+                  className="validate .center-align"
+                  onChange={this.props.handleInputChange}
+                />
+                <span
+                  className="helper-text"
+                  data-error="wrong"
+                  data-success="right"
+                >
+                  Email (this will be your username)
+                </span>
+              </div>
+
+              <div className="input-field ">
+                <Input
+                  id="password"
+                  type="password"
+                  name="password"
+                  placeholder="Enter your Password"
+                  autoComplete="off"
+                  onChange={this.props.handleInputChange}
+                />
+                <span
+                  className="helper-text"
+                  data-error="wrong"
+                  data-success="right"
+                >
+                  Password
+                </span>
+              </div>
+
+              <div className="input-field">
+                <Input
+                  placeholder="Enter url for your profile picture"
+                  s={6}
+                  name="image"
+                  id="image"
+                  onChange={this.props.handleInputChange}
+                  className=" .center-align"
+                />
+                <span
+                  className="helper-text"
+                  data-error="wrong"
+                  data-success="right"
+                >
+                  Profile Picture URL
+                </span>
+              </div>
+
+            </form>
+
+            <Button
+              className="signup-button white-text waves-effect waves-light btn #4a148c purple darken-4 z-depth-5"
+              onClick={this.props.newUser}
+            >
+              Sign Up
+            </Button>
+            <Button
+              className="back-to-login-button white-text waves-effect waves-light btn #4a148c purple darken-4 z-depth-5"
+              onClick={this.props.returnToLogin}
+            >
+              Back to login
+            </Button>
+          </div>
         </div>
-   <div className="input-field">
-          <input placeholder ="Enter your last name" name= "lastName" id="last-name"  className=" .center-align" onChange={props.handleInputChange}/>
-
-          <span className="helper-text" data-error="wrong" data-success="right" >Last Name</span>
-        </div>
-        <div className="input-field">
-          <input placeholder ="example@example.com" name = "username" id="email" type="email" className="validate .center-align" onChange={props.handleInputChange}/>
-
-          <span className="helper-text" data-error="wrong" data-success="right" >Email (this will be your username)</span>
-        </div>
-        <div className="input-field " >
-          <input id="password" type="password" name="password" placeholder ="Enter your Password" autoComplete="off" onChange={props.handleInputChange} />
-         
-          <span className="helper-text" data-error="wrong" data-success="right" >Password</span>
-        </div>
-       
-     
-    </form>
-  <div> {props.image }</div>
-   
-
-       <button className="signup-button white-text waves-effect waves-light btn #4a148c purple darken-4 z-depth-5" onClick={props.newUser}>Sign Up</button>
-       <button className = "back-to-login-button white-text waves-effect waves-light btn #4a148c purple darken-4 z-depth-5" onClick={props.returnToLogin}>Back to login</button> 
-    </div>
-    </div>
-);
+      </div>
+    );
+  }
+}
 
 export default NewUser;
--- client/src/components/Sidebar/User/index.js
+++ client/src/components/Sidebar/User/index.js
@@ -1,13 +1,20 @@
 import React from "react";
 import "./style.css";
+import { Card, CardTitle } from "react-materialize";
 
 function User(props) {
+  const imageURL = props.user.image ? props.user.image : "assets/images/SQ.png";
+  
   return (
-    <div id="profile-pic">
-      <img className="circle" src="http://i.pravatar.cc/300" alt="" />
-      User: {props.user.firstName} {props.user.lastName}
+    <div>
+        <Card horizontal id="profile-pic" className='small circle'
+            header={<CardTitle image={imageURL}></CardTitle>}>
+        {props.user.firstName} {props.user.lastName}
+        </Card>
     </div>
   );
 }
 
 export default User;
+
+
