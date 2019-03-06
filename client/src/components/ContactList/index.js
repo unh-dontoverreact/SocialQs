7073cde25673b6c3ac17177738e7bb2b7d89c0b2
Cheyra <staiercm@gmail.com>
2019-03-05 20:32:56 -0500

    travis fix

--- client/src/components/ContactList/index.js
+++ client/src/components/ContactList/index.js
@@ -8,50 +8,12 @@ export function ContactList({ children }) {
 }
 
 //Mark - deleteContact - possibly unneeded code
-<<<<<<< HEAD
 export function deleteContact(id) {
     console.log("Delete User", id);
-=======
-export function deleteContact() {
-  console.log("Delete User");
->>>>>>> d720c8091aa9a053fea11d803a518adabb896b0d
 }
 
 // ContactListItem - details of a contact
 export function ContactListItem(props) {
-<<<<<<< HEAD
-    return (
-        <li className="collection-item">
-            <Container>
-                <Row>
-                    <Col size="s12">
-                        <table className="striped responsive-table">
-                            <tbody>
-                                <tr>
-                                    <td>
-                                        {props.firstName} {props.lastName}
-                                    </td>
-                                    <td>{props.birthDate}</td>
-                                    <td>{props.email}</td>
-                                    <td>
-                                        {/* Mark - onClick to delete user  */}
-                                        <button
-                                            onClick={() => deleteContact(1)}
-                                        >
-                                            <i className="material-icons">
-                                                delete_forever
-                                            </i>
-                                        </button>
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </Col>
-                </Row>
-            </Container>
-        </li>
-    );
-=======
   return (
     <li className="collection-item">
       <Container>
@@ -82,5 +44,4 @@ export function ContactListItem(props) {
       </Container>
     </li>
   );
->>>>>>> d720c8091aa9a053fea11d803a518adabb896b0d
 }
--- client/src/pages/homePage.js
+++ client/src/pages/homePage.js
@@ -3,6 +3,7 @@ import { Col, Row, Container } from "../components/Grid";
 import Sidebar from "../components/Sidebar"
 import UserProfile from '../components/UserProfile';
 // import { EventList, EventListItem } from '../components/EventList';
+import { EventList } from '../components/EventList';
 import { Redirect } from 'react-router-dom'
 import NewEvent from '../components/NewEvent';
 import Axios from 'axios';
