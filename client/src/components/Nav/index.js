c6f9d6861bde919fcadf9918b05832190ad5fc8d
Laurie Anderson <plipandfirt@gmail.com>
2019-03-07 15:03:02 -0500

    added footer using styled-components & converted nav to styled-compontents

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,21 +1,46 @@
+// import React from "react";
+// import "./style.css";
+// import { Link } from "react-router-dom";
+
+// function Nav() {
+//   return (
+//     <nav>
+//       <div className="nav-wrapper">
+//         <ul id="nav-mobile" className="brand-logo center hide-on-med-and-down">
+//           <li>
+//             <Link to={`/`} activeclassname="material-icons waves-effect">
+//               SocialQs
+//             </Link>
+//           </li>
+//         </ul>
+//       </div>
+//     </nav>
+//   );
+// }
+
+// export default Nav;
+
 import React from "react";
-import "./style.css";
+import Styled from "styled-components";
 import { Link } from "react-router-dom";
 
-function Nav() {
-  return (
-    <nav>
-      <div className="nav-wrapper">
-        <ul id="nav-mobile" className="brand-logo center hide-on-med-and-down">
-          <li>
-            <Link to={`/`} activeclassname="material-icons waves-effect">
-              SocialQs
-            </Link>
-          </li>
-        </ul>
-      </div>
-    </nav>
-  );
-}
+const Nav = () => (
+    <NavBar>
+        <h5><Link to={`/`}>
+               SocialQs
+             </Link></h5>
+    </NavBar>
+);
 
+const NavBar = Styled.div `
+    width: 100%;
+    position:sticky;
+    top:0;
+    display:grid;
+    justify-items: center;
+    background-color: #612469;
+    box-shadow:0 14px 28px rgba(0,0,0,.25), 0 10px 10px rgba(0,0,0,.22);
+    color:white;
+    `
+    
 export default Nav;
