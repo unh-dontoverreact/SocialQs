012937b5b026a28f2e1f38b0c4cd82381c36f0a7
Mike Galarneau <mbgalarneau@gmail.com>
2019-03-08 18:52:04 -0500

    Fixed require error on app startup

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,9 +1,7 @@
 const router = require("express").Router();
-const contactRoutes = require("./contacts");
 const userRoutes = require("./users");
 
 // Contact API routes
-// router.use("/contacts", contactRoutes);
 router.use("/user", userRoutes);
 
 module.exports = router;
